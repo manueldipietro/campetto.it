6878268c6f4328edd100fbfac27d98d713e99000
LorisAbbruzzo <loris.abbruzzo1@gmail.com>
2024-10-17 22:54:21 +0200

    Modifiche a schema?

--- db/schema.rb
+++ db/schema.rb
@@ -43,17 +43,6 @@ ActiveRecord::Schema.define(version: 2024_10_14_172434) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "campos", force: :cascade do |t|
-    t.string "nome"
-    t.text "descrizione"
-    t.string "sport"
-    t.decimal "prezzo"
-    t.decimal "latitudine"
-    t.decimal "longitudine"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "fields", force: :cascade do |t|
     t.string "nome"
     t.text "descrizione"
@@ -82,11 +71,15 @@ ActiveRecord::Schema.define(version: 2024_10_14_172434) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "uid"
     t.string "provider"
     t.datetime "confirmed_at"
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
