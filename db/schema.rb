8240dd953d5fa064c8106397d3e6d02c294d88ab
manueldipietro <manuel.dipietro@outlook.com>
2024-10-17 21:34:41 +0200

    Implement partner model password, validation, email uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_17_190610) do
+ActiveRecord::Schema.define(version: 2024_10_17_193151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2024_10_17_190610) do
     t.string "mobile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_partners_on_email", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
